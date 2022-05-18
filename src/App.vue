05785e384cd1b6668066d5482a7cd5659fdea9ec
Bradley Smith <bradleyhop@gmail.com>
2022-05-18 12:06:04 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script></script>
+
+<template>
+  <main>
+    <!-- Sidebar start -->
+
+    My Documents
+
+    New Document
+
+    <!-- Sidebar end -->
+
+    <!-- Header start -->
+
+    Document Name
+
+    Save Changes
+
+    <!-- Header end -->
+
+    <!-- Main start -->
+
+    Markdown
+
+    Preview
+
+    <!-- Main end -->
+  </main>
+</template>
+
+<style></style>
